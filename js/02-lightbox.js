913edde446fff944bd2f6bcba0fcfd897d6d2f40
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-08-06 21:39:11 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let lightbox = new SimpleLightbox(".gallery li a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  lightbox.on("show.simplelightbox", function () {
+    //
+  });
+}
